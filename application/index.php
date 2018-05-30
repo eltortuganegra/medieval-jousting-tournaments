de47aa9336bf2fd948c4e621512a1f34b88f2efd
eltortuganegra <jorge.sanchez@eltortuganegra.com>
2018-05-30 12:02:50 +0200

    Enable subdomain for eltortuganegra.com

--- application/index.php
+++ application/index.php
@@ -27,6 +27,6 @@ $config = $environment->getConfigPath();
 //}
 
 // change the following paths if necessary
-$yii = $rootPath . '/../../yii/framework/yii.php';
+$yii = $rootPath . '/../../yii-1.1.13.e9e4a0/framework/yii.php';
 require_once($yii);
 Yii::createWebApplication($config)->run();
