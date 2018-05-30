0c0ad06fa685de17dd3950da0310cf031ac1609c
eltortuganegra <jorge.sanchez@eltortuganegra.com>
2018-05-30 12:05:43 +0200

    Enable subdomain for eltortuganegra.com

--- application/index.php
+++ application/index.php
@@ -1,32 +1,15 @@
 <?php
 
 $rootPath = dirname(__FILE__);
-
 require($rootPath . '/protected/components/Environment.php');
 
 $host = $_SERVER['HTTP_HOST'];
 $environment = new Environment($host, $rootPath);
 $environment->buildConfiguration();
-$config = $environment->getConfigPath();
+$configPath = $environment->getConfigPath();
 
-//Load config development or production
-//if ($environment->isProductionEnvironment()) {
-//        $config=dirname(__FILE__).'/protected/config/production.php';
-//} else if ($environment->isPreProduction()) {
-//        $config=dirname(__FILE__).'/protected/config/pre-production.php';
-//        // remove the following lines when in production mode
-//        defined('YII_DEBUG') or define('YII_DEBUG',true);
-//        // specify how many levels of call stack should be shown in each log message
-//        defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-//} else {
-//        $config=dirname(__FILE__).'/protected/config/development.php';
-//        // remove the following lines when in production mode
-//        defined('YII_DEBUG') or define('YII_DEBUG',true);
-//        // specify how many levels of call stack should be shown in each log message
-//        defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-//}
 
-// change the following paths if necessary
-$yii = $rootPath . '/../../yii-1.1.11.58da45/framework/yii.php';
-require_once($yii);
-Yii::createWebApplication($config)->run();
+$yiiPath = $rootPath . '/../../yii-1.1.11.58da45/framework/yii.php';
+require_once($yiiPath);
+
+Yii::createWebApplication($configPath)->run();
